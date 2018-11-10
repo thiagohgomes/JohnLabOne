f1186c731af6b0caa4c75dd18605a14b7b210e67
thiagohgomes <44788547+thiagohgomes@users.noreply.github.com>
2018-11-10 17:43:33 -0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
 
 //modules
 var express = require('express');
-var consign = require('consign');
+/*var consign = require('consign');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
-
+*/
 var app = express();
-
+/*
 //views
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
@@ -18,11 +18,12 @@ app.use(expressValidator());
 
 
 //inludes
+
 consign()
 	.include('app/routes')
 	.then('app/models')
 	.then('app/controllers')
 	.into(app);
-
+*/
 
 module.exports = app;
