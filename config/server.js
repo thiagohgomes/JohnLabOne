a16d065a1a88d9674f195e8c42cbc6e5303f4b2a
thiagohgomes <44788547+thiagohgomes@users.noreply.github.com>
2018-11-10 17:47:27 -0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 //middlewares
-app.use(express.static('./app/public'));
+//app.use(express.static('./app/public'));
 //app.use(bodyParser.urlencoded({extended: true}));
 //app.use(expressValidator());
 
