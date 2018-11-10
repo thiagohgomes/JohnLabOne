1eb7deef243162d6dc2bfa836801754473a67805
thiagohgomes <44788547+thiagohgomes@users.noreply.github.com>
2018-11-10 17:44:01 -0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 
 //modules
 var express = require('express');
-/*var consign = require('consign');
+var consign = require('consign');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
-*/
+
 var app = express();
 /*
 //views
