89a17594dde484186e9be163bc41b04249480529
thiagohgomes <44788547+thiagohgomes@users.noreply.github.com>
2018-11-10 16:58:20 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+    
+}).listen(process.env.PORT || 8080);
+/*
 var app = require('./config/server');
 app.listen(80, function(){
 	console.log('servidor online');
 })
+*/
