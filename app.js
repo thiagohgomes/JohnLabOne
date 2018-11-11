eaf950f1926be66a056a36fa7ca948c2b56ec2b0
Thiago <thiagohgomes84@gmail.com>
2018-11-11 13:56:52 -0200

    rowback para a versão funcional do meu app

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-/*
 var app = require('./config/server');
-app.listen(process.env.PORT || 8080, function(){
+app.listen(80, function(){
 	console.log('servidor online');
 })
-*/
-
-var app = require('./config/server');
-app.listen(process.env.PORT || 8080, function(){});
-
-
-/*var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, worlds!');
-    
-}).listen(process.env.PORT || 8080);
-*/
--- config/server.js
+++ config/server.js
@@ -1,29 +1,28 @@
 
 //modules
 var express = require('express');
-//var consign = require('consign');
-//var bodyParser = require('body-parser');
-//var expressValidator = require('express-validator');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 
 //views
-//app.set('view engine', 'ejs');
-//app.set('views', './app/views');
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
 
 //middlewares
-//app.use(express.static('./app/public'));
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(expressValidator());
+app.use(express.static('./app/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 
 //inludes
-
-/*consign()
+consign()
 	.include('app/routes')
 	.then('app/models')
 	.then('app/controllers')
 	.into(app);
-*/
+
 
 module.exports = app;
