afeb75fcb806e6ee54344977388cb89fffa8af7e
Thiago <thiagohgomes84@gmail.com>
2018-11-15 22:23:06 -0200

    1.7 - mysql conection

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,22 @@
 module.exports = function(app){
-	app.get('/noticias', function(req,res){
-	    res.render('noticias/noticias');
-	});
-}
+    app.get('/noticias', function(req,res){
 
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'node_mobile.mysql.dbaas.com.br',
+            user: 'node_mobile',
+            password: 'Lancamento@31',
+            database: 'node_mobile'
+        });
+
+        connection.query("SELECT * FROM noticias", function(error, result){
+            if(result != null)
+            	res.send(result);
+            else
+            	res.send(error);
+        });
+
+        //res.render('noticias/noticias');
+    });
+}
